27aa0fa60c5c1c2eb4b2c5798cff5be25a39a8f0
Huy Nguyen <nguyenduchuy27111997@gmail.com>
2025-04-25 20:28:09 -0700

    Add initial project files including package-lock.json, multiple content articles, and images for bootcamp and data analysis courses

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,28 +2,29 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
+  name: "Huy Nguyen",
+  initials: "HN",
   url: "https://dillion.io",
   location: "San Francisco, CA",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer turned Indie Hacker. I love building things and helping people.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [progressed through various roles in my career](/#career). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
-  avatarUrl: "/me.png",
+  avatarUrl: "/me.jpg",
   skills: [
     "React",
+    "React Native",
     "Next.js",
     "Typescript",
+    "Expo",
     "Node.js",
     "Python",
-    "Go",
     "Postgres",
     "Docker",
-    "Kubernetes",
-    "Java",
-    "C++",
+    "AWS",
+    "CI/CD",
+    "Git",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
